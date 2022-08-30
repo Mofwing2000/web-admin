9917e940939bd19947e39a5695f44eac11499da9
minhvd <minhvd@vmodev.com>
2022-08-31 01:56:44 +0700

    MinhVD Refactor replace any type

--- src/routes/AdminRoute.tsx
+++ src/routes/AdminRoute.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../helpers/hooks';
+import AuthState from '../models/auth';
+import { UserState } from '../models/user';
 import { selectAuth } from '../store/root-reducer';
+
+import { selectUser } from '../store/user/user.reducer';
 
 interface IProps {
     children: JSX.Element;
 }
 
 const AdminRoute = (props: IProps) => {
-    const { userToken, currentUser } = useAppSelector<any>(selectAuth);
-    if (userToken !== null) {
-        if (currentUser.role === 'admin') return props.children;
+    const { userToken } = useAppSelector<AuthState>(selectAuth);
+    const { user } = useAppSelector<UserState>(selectUser);
+    if (userToken !== null && user !== null) {
+        if (user.role === 'admin') return props.children;
         else return <Navigate to={'/'}></Navigate>;
     } else return <Navigate to={'/login'}></Navigate>;
 };
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../helpers/hooks';
+import AuthState from '../models/auth';
 import { selectAuth } from '../store/root-reducer';
 
 interface IProps {
@@ -8,7 +9,7 @@ interface IProps {
 }
 
 const PrivateRoute = (props: IProps) => {
-    const { userToken } = useAppSelector<any>(selectAuth);
+    const { userToken } = useAppSelector<AuthState>(selectAuth);
     if (userToken !== null) return props.children;
     else return <Navigate to={'/login'}></Navigate>;
 };
