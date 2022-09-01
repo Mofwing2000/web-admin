efc21b86973eb59a5f2e5cd64711744c04d448d5
minhvd <minhvd@vmodev.com>
2022-09-01 10:01:16 +0700

    MinhVD Add forgot password input

--- src/models/form.ts
+++ src/models/form.ts
@@ -1,17 +1,8 @@
-// import { UserRole } from '../type/UserType';
-
-// export interface UserManageFormValues {
-//     email: string;
-//     firstName: string;
-//     lastName: string;
-//     password: string;
-//     photoUrl: string;
-//     phoneNumber: string;
-//     address: string;
-//     role: UserRole;
-// }
-
 export interface LoginInput {
     email: string;
     password: string;
+}
+
+export interface ForgotPasswordInput {
+    email: string;
 }
