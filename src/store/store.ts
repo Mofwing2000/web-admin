94244fdbd9d785db30205d38313188f5de1a1fe0
minhvd <minhvd@vmodev.com>
2022-09-01 09:52:38 +0700

    MinhVD Add rootSage to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,14 +3,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authenticateSaga } from './auth/auth.saga';
 // import { authenticateSaga } from '../app/saga/authenticate.saga';
 import rootReducer from './root-reducer';
+import rootSaga from './rootSaga';
 import { createReduxHistory, routerMiddleware } from './router/router-reducer';
 const sagaMiddleware = createSagaMiddleware();
+
 export const store = configureStore({
     reducer: rootReducer,
     middleware: [sagaMiddleware, routerMiddleware],
 });
 
-sagaMiddleware.run(authenticateSaga);
+sagaMiddleware.run(rootSaga);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export const history = createReduxHistory(store);
