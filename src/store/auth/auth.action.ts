c00d721ea57482b78d4a0f59955052b0840f2e2c
minhvd <minhvd@vmodev.com>
2022-09-01 09:56:12 +0700

    MinhVD Delete unnecessary signup

--- src/store/auth/auth.action.ts
+++ src/store/auth/auth.action.ts
@@ -1,6 +1,6 @@
 import { User } from 'firebase/auth';
 import { createAction, createAsyncAction } from 'typesafe-actions';
-import { LoginInput, SignupInput } from '../../models/form';
+import { LoginInput } from '../../models/form';
 import { AuthActionsType } from '../../type/auth';
 
 export const loginAsync = createAsyncAction(
@@ -8,11 +8,5 @@ export const loginAsync = createAsyncAction(
     AuthActionsType.LOGIN_START_SUCCEED,
     AuthActionsType.LOGIN_START_FAIL,
 )<LoginInput, { token: string }, string>();
-
-export const signupAsync = createAsyncAction(
-    AuthActionsType.SIGNUP_START,
-    AuthActionsType.SIGNUP_START_SUCCEED,
-    AuthActionsType.SIGNUP_START_FAIL,
-)<SignupInput, void, string>();
 
 export const logout = createAction(AuthActionsType.LOGOUT)();
