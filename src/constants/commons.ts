2ee4bdaa96cb862d472b61fed26af669f2644aca
minhvd <minhvd@vmodev.com>
2022-09-05 02:30:34 +0700

    MinhVD Add month name to common constant

--- src/constants/commons.ts
+++ src/constants/commons.ts
@@ -4,3 +4,17 @@ export const DEFAULT_PRODUCT_PHOTO_URL =
     'https://firebasestorage.googleapis.com/v0/b/online-shop-c6693.appspot.com/o/productPhotos%2Fistockphoto-1202373625-612x612.jpg?alt=media&token=cfdc4dac-d215-4e73-9299-f34ea48f23be';
 export const DEFAULT_COLLECTION_PHOTO_URL =
     'https://firebasestorage.googleapis.com/v0/b/online-shop-c6693.appspot.com/o/collectionPhotos%2F00_story.jpeg?alt=media&token=7a07118c-0069-4821-8d48-496d0c016263';
+export const monthLabel = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
